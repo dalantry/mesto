7bd36aba3c55757c49180ac026290a1b763e40a9
Ilya <glkz.pisligin@gmail.com>
2020-11-25 15:26:06 +0300

    после ревью

--- script.js
+++ script.js
@@ -3,51 +3,37 @@ let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit-button");
 let popupClose = document.querySelector(".popup__close");
 
-function openForm(){
-    popup.classList.add('popup_opened');
+function openForm() {
+  popup.classList.add("popup_opened");
 }
 
-function closeForm(){
-    popup.classList.remove('popup_opened');
+function closeForm() {
+  popup.classList.remove("popup_opened");
+  nameInput.value = document.querySelector(".profile__title").textContent;
+  jobInput.value = document.querySelector(".profile__text").textContent;
 }
 
-editButton.addEventListener('click', openForm);
-popupClose.addEventListener('click', closeForm);
+editButton.addEventListener("click", openForm);
+popupClose.addEventListener("click", closeForm);
 
-// Информация о себе
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__main');
-let nameInput = formElement.querySelector('.popup__text-name');
-let jobInput = formElement.querySelector('.popup__text-job');
+// Текст формы
+let formElement = document.querySelector(".popup__main");
+let nameInput = formElement.querySelector(".popup__text-name");
+let jobInput = formElement.querySelector(".popup__text-job");
 
-nameInput.value = document.querySelector('.profile__title').textContent;
-jobInput.value = document.querySelector('.profile__text').textContent;
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
+nameInput.value = document.querySelector(".profile__title").textContent;
+jobInput.value = document.querySelector(".profile__text").textContent;
 
-    // Находим поля формы в DOM
-    
+function formSubmitHandler(evt) {
+  evt.preventDefault(); 
 
-    // Получите значение полей из свойства value
-    
+  let name = document.querySelector(".profile__title");
+  let job = document.querySelector(".profile__text");
 
-    // Выберите элементы, куда должны быть вставлены значения полей 
-    let name = document.querySelector('.profile__title');
-    let job = document.querySelector('.profile__text');
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
 
-    // Вставьте новые значения с помощью textContent
-    name.textContent = nameInput.value; 
-    job.textContent = jobInput.value;
-
-    closeForm();
+  closeForm();
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
-
+formElement.addEventListener("submit", formSubmitHandler);
