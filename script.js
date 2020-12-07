b0d1b4610e13d83be5dd5044e05c13221579c58e
Ilya <glkz.pisligin@gmail.com>
2020-12-07 12:24:08 +0300

    сделано: форма добавления и лайк

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function formSubmitHandler(evt) {
 }
 formElement.addEventListener("submit", formSubmitHandler);
 
-//Добавление новой карточки
+//Форма добавляения
 const addButton = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup__add-card');
 const popupCloseAdd = document.querySelector('.popup__close_add');
@@ -55,3 +55,79 @@ function closeFormAdd() {
 addButton.addEventListener("click", openFormAdd);
 popupCloseAdd.addEventListener("click", closeFormAdd);
 
+// Cards
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const templateElement = document.querySelector('.template');
+const listContainer = document.querySelector('.cards__elements');
+
+// Карточки из коробки
+function newCard(item){
+  const newItem = templateElement.content.cloneNode(true);
+  const titleElement = newItem.querySelector('.cards__text');
+  titleElement.textContent = item.name;
+  const linkElement = newItem.querySelector('.cards__image');
+  linkElement.setAttribute('src', item.link);
+
+  return newItem;
+}
+
+function renderCards(){
+  const listCards = initialCards.map(newCard);
+
+  listContainer.append(...listCards);
+}
+
+renderCards();
+
+// добавление новых карточек
+const inputElementTitle = document.querySelector('.popup__input_text_place');
+const inputElementLink = document.querySelector('.popup__input_text_link');
+
+function addNewCard(event){
+    event.preventDefault();
+
+    const inputTitle = inputElementTitle.value;
+    const inputLink = inputElementLink.value;
+    const newItemCard = newCard({ name: inputTitle, link: inputLink});
+    listContainer.prepend(newItemCard);
+
+    closeFormAdd();
+  }
+
+popupAdd.addEventListener('submit', addNewCard);
+
+//кнопка лайк
+
+const likeButton = document.querySelector('.cards__like-button');
+
+function like(){
+  likeButton.classList.toggle('cards__like-button_active');
+};
+
+likeButton.addEventListener('click', like);
