f00dde42b8f93314f2103149c6922b51acdcbe8d
Ilya <glkz.pisligin@gmail.com>
2020-12-07 12:59:08 +0300

    feat: little detail css

--- script.js
+++ script.js
@@ -58,20 +58,20 @@ popupCloseAdd.addEventListener("click", closeFormAdd);
 // Cards
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1533193256116-fcaf17cfb3b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: 'Монрепо',
+      link: 'https://images.unsplash.com/photo-1536012354193-8bb300dc3ce6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Токсово',
+      link: 'https://images.unsplash.com/photo-1517940094524-be03e5da7a98?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      name: 'Новороссийск',
+      link: 'https://images.unsplash.com/photo-1523175347343-52d8867ca0bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
   },
   {
       name: 'Холмогорский район',
